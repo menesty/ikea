51a267742eb00e1090234c07cbbe2f0b3cea3b7b
Menesty <ahnezdyuk@gmail.com>
2013-11-28 18:15:49 +0200

    Tablet Version

---
+++ tablet/src/org/menesty/ikea/tablet/TabletActivity.java
@@ -0,0 +1,15 @@
+package org.menesty.ikea.tablet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabletActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
